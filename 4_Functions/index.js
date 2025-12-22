5bcf6189c5c4689fc7923657b9cd5a0ede63865b
Aditya Prabhulkar <adityaprabhulkar2000@gmail.com>
2025-12-22 22:22:54 +0530

    normal fynction , function expression, arrow function

---
+++ 4_Functions/index.js
@@ -0,0 +1,83 @@
+// =================={ Functions } -======================
+
+// function defination
+// function sayMyName() {
+//   console.log("Aditya Prabhulkar ");
+// }
+
+// // function use - function call
+// sayMyName();
+
+// function printCounting() {
+//   for (i = 1; i <= 100; i++) {
+//     console.log(i);
+//   }
+// }
+
+// printCounting();
+
+// function printingNumber(num) {
+//     console.log("Printing Number : " + num);
+// }
+
+// printingNumber(29)
+
+// function getSum (a, b, c) {
+//        let sum = a + b + c
+//         return sum;
+
+// }
+// let ans = getSum(5, 5, 5);
+// console.log(ans);
+
+// function getMyName(firstName, lastName){
+//     let fullName = firstName  + " "+ lastName;
+//     return fullName;
+// }
+
+// let fullName = getMyName(7, 7);
+// console.log(fullName);
+
+
+// -----------------------------------------------------
+
+
+// ------------------------------// function expression------------------
+
+// const getMultification = function (a, b) {
+//   return a * b;
+// };
+
+// let ans = getMultification(2, 20);
+
+// console.log(ans);
+
+// const squreNumber = function(num) {
+//     let ans = num **2;
+//     return ans
+// }
+
+// let ans = squreNumber(5);
+// console.log(ans);
+
+// function getExp(x,y) {
+//     let ans = x**y;
+//     return ans;
+// }
+
+// console.log(getExp(2,10));
+
+// const getExp = function (x,y) {
+//     let ans = x**y;
+//     return ans;
+// }
+
+// console.log(getExp(2,10));
+
+// const getExp =  (x,y) => {
+//     let ans = x**y;
+//     return ans;
+// }
+
+// console.log(getExp(2,10));
+
