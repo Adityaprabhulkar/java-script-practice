a9570d76974858d3ec4107454465e545caab60e7
Aditya Prabhulkar <adityaprabhulkar2000@gmail.com>
2025-12-24 00:45:18 +0530

    practice about default parameters

---
+++ 9_default_parameters_functions/index.js
@@ -0,0 +1,98 @@
+// -----------------{ Default parameter}----------------
+
+// function sayName(fName = "raj", lName = fName.toUpperCase()) {
+//     console.log("My Name is ", fName, " ", lName);
+    
+// }
+
+// sayName("Aditya")
+
+
+// function solve(value = {age:15,wt:90,ht:190}) {
+//     console.log("HEllo jee", value);
+    
+// }
+
+// solve()
+
+// function solve(value =  "Rahul") {
+//     console.log("HEllo jee", value);
+    
+// }
+
+// solve(undefined)
+
+
+function getAge() {
+    return 14;
+}
+
+function utiity(name = "Aditya", age=getAge() ) {
+    console.log(name, " ", age);
+    
+}
+
+utiity();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// class Human {
+//     //properties
+//     age = 13;
+//     #wt = 30;
+//     ht = 180;
+
+
+//     constructor(newAge, newHeight, newWeight) {
+//         this.age = newAge;
+//         this.ht = newHeight;
+//         this.#wt = newWeight;
+//      }
+
+//     //behaviour
+
+//     walking() {
+//         console.log("I am walking", this.#wt);
+//     }
+
+//     running() {
+//         console.log("I am running");
+//     }
+
+//     get fetchWeight() {  // getter
+//         return this.#wt
+//     }
+
+//     set modifyWeight(val) { // modify data
+//         this.#wt = val;
+//     }
+// }
+
+// let obj = new Human(25, 180, 90);
+// console.log(obj.fetchWeight);
+
+
+
+// obj.walking()
+
+
+
+
+
+
+
+
+
+
+
