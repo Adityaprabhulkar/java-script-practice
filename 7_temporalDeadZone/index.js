9c28e705391dce9f8cbfe9c4b1bd2678ec71aab3
Aditya Prabhulkar <adityaprabhulkar2000@gmail.com>
2025-12-23 23:41:45 +0530

    temporal dead zone & blocked scope

---
+++ 7_temporalDeadZone/index.js
@@ -0,0 +1,51 @@
+
+console.log(marks);
+
+console.log("aditya");
+console.log("love");
+
+
+const marks = 100
+console.log(marks);
+
+
+
+
+// {
+
+//     var name = "raj"
+// }
+// console.log(name);
+
+
+
+// function sayHello() {
+//     var fullName = "Aditya"
+//     console.log(fullName);
+    
+// }
+// console.log(fullName);
+
+// sayHello()
+
+
+
+// var age = 18;
+
+// {
+//   console.log(age);
+// }
+
+// if (true) {
+//   console.log(age);
+// }
+
+// for (let i = 0; i <= 2; i++) {
+//   console.log(age);
+// }
+
+// function print() {
+//   console.log("this is raj", age);
+// }
+
+// print();
