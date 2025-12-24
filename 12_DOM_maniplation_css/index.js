75f26440a13711796a53495edfd495bcdaf2c156
Aditya Prabhulkar <adityaprabhulkar2000@gmail.com>
2025-12-25 01:26:17 +0530

    practing DOM manipulation in CSS

---
+++ 12_DOM_maniplation_css/index.js
@@ -0,0 +1,67 @@
+// // let paraElement = document.getElementById("spara")
+// // console.log(paraElement.style);
+
+// // paraElement.style.backgroundColor = "blue";
+// // paraElement.style.color = "white";
+
+// // let secondElement = document.getElementById("sdiv");
+// // console.log(secondElement);
+
+// // secondElement.style.cssText;
+// // secondElement.style.cssText = "background-Color: black; color: white; padding: 0.5rem"
+
+
+// // let firstdiv = document.querySelector('#fdiv');
+// // console.log(firstdiv);
+
+// // firstdiv.setAttribute('class', 'divClass')
+
+// // console.log(firstdiv);
+
+// // firstdiv.setAttribute('class', 'secondDivClass')
+
+// // console.log(firstdiv);
+
+// // firstdiv.setAttribute('style', "background-color: yellow; padding: 0.4rem")
+
+
+let fpara = document.querySelector("#fpara");
+fpara.setAttribute('style', 'margin:1rem')
+
+let fpara = document.querySelector("#fpara");
+fpara.className= "Gohan bulbul"
+
+console.log(fpara);
+// 
+
+let fpara = document.querySelector("#fpara");
+console.log(fpara);
+
+console.log(fpara.classList);
+
+fpara.classList.add('thirdClass')
+
+console.log(fpara.classList);
+
+
+fpara.classList.remove('firstClass')
+console.log(fpara.classList);
+
+fpara.classList.remove('thirdClass')
+console.log(fpara.classList);
+
+
+fpara.classList.toggle('thirdClass')
+fpara.classList.toggle('firstClass')
+console.log(fpara.classList);
+
+fpara.classList.toggle('fourthClass')
+console.log(fpara.classList);
+
+
+fpara.classList.contains("firstClass");
+// console.log(fpara.classList);
+console.log(fpara);
+
+
+
