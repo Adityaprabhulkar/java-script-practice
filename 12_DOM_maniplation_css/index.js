8d567e8070d5482be7b32b281aaeea8f60992032
Aditya Prabhulkar <adityaprabhulkar2000@gmail.com>
2025-12-25 01:28:41 +0530

    update

--- 12_DOM_maniplation_css/index.js
+++ 12_DOM_maniplation_css/index.js
@@ -1,67 +1,67 @@
-// // let paraElement = document.getElementById("spara")
-// // console.log(paraElement.style);
+// // // let paraElement = document.getElementById("spara")
+// // // console.log(paraElement.style);
 
-// // paraElement.style.backgroundColor = "blue";
-// // paraElement.style.color = "white";
+// // // paraElement.style.backgroundColor = "blue";
+// // // paraElement.style.color = "white";
 
-// // let secondElement = document.getElementById("sdiv");
-// // console.log(secondElement);
+// // // let secondElement = document.getElementById("sdiv");
+// // // console.log(secondElement);
 
-// // secondElement.style.cssText;
-// // secondElement.style.cssText = "background-Color: black; color: white; padding: 0.5rem"
-
-
-// // let firstdiv = document.querySelector('#fdiv');
-// // console.log(firstdiv);
-
-// // firstdiv.setAttribute('class', 'divClass')
-
-// // console.log(firstdiv);
-
-// // firstdiv.setAttribute('class', 'secondDivClass')
-
-// // console.log(firstdiv);
-
-// // firstdiv.setAttribute('style', "background-color: yellow; padding: 0.4rem")
+// // // secondElement.style.cssText;
+// // // secondElement.style.cssText = "background-Color: black; color: white; padding: 0.5rem"
 
 
-let fpara = document.querySelector("#fpara");
-fpara.setAttribute('style', 'margin:1rem')
+// // // let firstdiv = document.querySelector('#fdiv');
+// // // console.log(firstdiv);
 
-let fpara = document.querySelector("#fpara");
-fpara.className= "Gohan bulbul"
+// // // firstdiv.setAttribute('class', 'divClass')
 
-console.log(fpara);
-// 
+// // // console.log(firstdiv);
 
-let fpara = document.querySelector("#fpara");
-console.log(fpara);
+// // // firstdiv.setAttribute('class', 'secondDivClass')
 
-console.log(fpara.classList);
+// // // console.log(firstdiv);
 
-fpara.classList.add('thirdClass')
-
-console.log(fpara.classList);
+// // // firstdiv.setAttribute('style', "background-color: yellow; padding: 0.4rem")
 
 
-fpara.classList.remove('firstClass')
-console.log(fpara.classList);
+// let fpara = document.querySelector("#fpara");
+// fpara.setAttribute('style', 'margin:1rem')
 
-fpara.classList.remove('thirdClass')
-console.log(fpara.classList);
+// let fpara = document.querySelector("#fpara");
+// fpara.className= "Gohan bulbul"
 
+// console.log(fpara);
+// // 
 
-fpara.classList.toggle('thirdClass')
-fpara.classList.toggle('firstClass')
-console.log(fpara.classList);
+// let fpara = document.querySelector("#fpara");
+// console.log(fpara);
 
-fpara.classList.toggle('fourthClass')
-console.log(fpara.classList);
-
-
-fpara.classList.contains("firstClass");
 // console.log(fpara.classList);
-console.log(fpara);
+
+// fpara.classList.add('thirdClass')
+
+// console.log(fpara.classList);
+
+
+// fpara.classList.remove('firstClass')
+// console.log(fpara.classList);
+
+// fpara.classList.remove('thirdClass')
+// console.log(fpara.classList);
+
+
+// fpara.classList.toggle('thirdClass')
+// fpara.classList.toggle('firstClass')
+// console.log(fpara.classList);
+
+// fpara.classList.toggle('fourthClass')
+// console.log(fpara.classList);
+
+
+// fpara.classList.contains("firstClass");
+// // console.log(fpara.classList);
+// console.log(fpara);
 
 
 
