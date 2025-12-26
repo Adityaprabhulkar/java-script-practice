c9acaa2aa2e400ba972c1e8f8476e945d14014ce
Aditya Prabhulkar <adityaprabhulkar2000@gmail.com>
2025-12-27 00:08:58 +0530

    update

--- 16_Promises_js/index.js
+++ 16_Promises_js/index.js
@@ -65,21 +65,53 @@
 // })
 
 
-let promise1 = new Promise((resolve, reject) => {
-    setTimeout(resolve, 1000, "First");
-})
-let promise2 = new Promise((resolve, reject) => {
-    setTimeout(resolve, 2000, "Second");
-})
-let promise3 = new Promise((resolve, reject) => {
-    setTimeout(reject, 4000, "Third");
-})
+// let promise1 = new Promise((resolve, reject) => {
+//     setTimeout(resolve, 1000, "First");
+// })
+// let promise2 = new Promise((resolve, reject) => {
+//     setTimeout(resolve, 2000, "Second");
+// })
+// let promise3 = new Promise((resolve, reject) => {
+//     setTimeout(reject, 4000, "Third");
+// })
 
-Promise.all([promise1, promise2, promise3])
-.then((value) => {
-    console.log(value);
+// Promise.all([promise1, promise2, promise3])
+// .then((value) => {
+//     console.log(value);
     
-}).catch((err)=> {
-    console.log("err:" + err);
+// }).catch((err)=> {
+//     console.log("err:" + err);
     
-})
+// })
+
+// const promise1 = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     resolve("foo");
+//   }, 3000);
+// });
+
+// promise1.then((value) => {
+//   console.log(value);
+//   // Expected output: "foo"
+// });
+
+// console.log(promise1);
+// // Expected output: [object Promise]
+
+const url = "https://jsonplaceholder.typicode.com/posts/1";
+
+fetch(url)
+    
+
+    .then((Response) => {
+        // console.log("get Data ", data);/
+        return Response.json();
+    })
+
+    .then((data) => {
+        console.log("get Data ", data);
+    })
+    .catch((err) => {
+        console.log("error: ", err);
+        
+    })
