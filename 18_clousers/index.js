f45db3835d585246efaee5cde469cc16472ae206
Aditya Prabhulkar <adityaprabhulkar2000@gmail.com>
2025-12-28 20:30:19 +0530

    clousers in java Script

--- 18_clousers/index.js
+++ 18_clousers/index.js
@@ -1,1 +1,12 @@
 // ------------##{clousers}--------
+let name = "Rahul"
+function outerFn() {
+    // let name = "aditya";
+    function innerFn() {
+        // let name = "rohit"
+        console.log(name);
+    }
+    innerFn()
+}
+
+outerFn()
