0dd89f28ab7983e31c935e263545b4c342d165f9
Aditya Prabhulkar <adityaprabhulkar2000@gmail.com>
2025-12-30 22:30:51 +0530

    See about promise chaning and attach to function wth .then

--- 20_promise-p2/index.js
+++ 20_promise-p2/index.js
@@ -1,5 +1,53 @@
+// const cart = ["shoes", "pants", "kurtas"];
+
+// createOrder(cart, function (orderId) {
+//   proceedToPayment(orderId);
+// });
+
+// const promise = createOrder(cart);
+
+// promise.then(function (orderId) {
+//   proceedToPayment(orderId);
+// });
+
+// ===================
+
+// const GITHUB_API = "https://api.github.com/users/akshaymarch7";
+
+// const user = fetch(GITHUB_API);
+
+// console.log(user);
+
+// user.then(function (data) {
+//   console.log(data);
+// });
+
+// ==================================
+
 const cart = ["shoes", "pants", "kurtas"];
 
-createOrder(cart, function(orderId) {
-    proceedToPayment(orderId);
-})
+// createOrder(cart, function (orderId) {
+//   proceedToPayment(orderId, function (paymentInfo) {
+//     showOrderSummary(paymentInfo, function () {
+//       updateWalletBalance();
+//     });
+//   });
+// });
+
+createOrder(cart)
+  .then(function (orderId) {
+    return proceedToPayment(orderId);
+  })
+  .then(function (paymentInfo) {
+    return showOrderSummary(paymentInfo);
+  })
+  .then(function (paymentInfo) {
+    return updateWalletBalance(paymentInfo);
+  });
+
+//   =====================================
+// createOrder(cart)
+//   .then((orderId) => proceedToPayment(orderId))
+//   .then((paymentInfo) => showOrderSummary(paymentInfo))
+//   .then((paymentInfo) => updateWalletBalance(paymentInfo));
+  
