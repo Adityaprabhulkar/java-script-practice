884e846c359acd8339fec3d569e1a6d79698865a
Aditya Prabhulkar <adityaprabhulkar2000@gmail.com>
2026-01-03 22:13:17 +0530

    update

--- 22_promise_api/index.js
+++ 22_promise_api/index.js
@@ -46,43 +46,43 @@
 //     console.error(err);
 //   });
 
-// // ==================={Promise.race()}============================
-// const p1 = new Promise((resolve, reject) => {
-//   setTimeout(() => resolve("p1 Sucess"), 3000);
-// });
-
-// const p2 = new Promise((resolve, reject) => {
-//   setTimeout(() => resolve("p2 success"), 5000);
-// //   setTimeout(() => reject("p2 Fail"), 1000);
-// });
-
-// const p3 = new Promise((resolve, reject) => {
-// //   setTimeout(() => resolve("p3 Sucess"), 2000);
-//   setTimeout(() => reject("p3 fail"), 2000);
-// });
-
-// Promise.race([p1, p2, p3])
-//   .then((res) => {
-//     console.log(res);
-//   })
-//   .catch((err) => {
-//     console.error(err);
-//   });
-
-// ==================={Promise.any()}============================
+// ==================={Promise.race()}============================
 const p1 = new Promise((resolve, reject) => {
-//   setTimeout(() => resolve("p1 Sucess"), 3000);
-    setTimeout(() => reject("p1 fail"), 3000);  
+  setTimeout(() => resolve("p1 Sucess"), 3000);
 });
 
 const p2 = new Promise((resolve, reject) => {
-//   setTimeout(() => resolve("p2 success"), 5000);
-  setTimeout(() => reject("p2 Fail"), 1000);
+  setTimeout(() => resolve("p2 success"), 5000);
+//   setTimeout(() => reject("p2 Fail"), 1000);
 });
 
 const p3 = new Promise((resolve, reject) => {
 //   setTimeout(() => resolve("p3 Sucess"), 2000);
   setTimeout(() => reject("p3 fail"), 2000);
+});
+
+Promise.race([p1, p2, p3])
+  .then((res) => {
+    console.log(res);
+  })
+  .catch((err) => {
+    console.error(err);
+  });
+
+// ==================={Promise.any()}============================
+const p4 = new Promise((resolve, reject) => {
+//   setTimeout(() => resolve("p1 Sucess"), 3000);
+    setTimeout(() => reject("p4 fail"), 3000);  
+});
+
+const p5 = new Promise((resolve, reject) => {
+//   setTimeout(() => resolve("p2 success"), 5000);
+  setTimeout(() => reject("p5 Fail"), 1000);
+});
+
+const p6 = new Promise((resolve, reject) => {
+//   setTimeout(() => resolve("p3 Sucess"), 2000);
+  setTimeout(() => reject("p6 fail"), 2000);
 });
 
 Promise.any([p1, p2, p3])
@@ -94,4 +94,6 @@ Promise.any([p1, p2, p3])
     console.log(err.errors);
     
   });
+
+  // ========================
 
